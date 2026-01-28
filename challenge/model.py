9d6fbae0384129830190b720c9595909df9159fa
Cristóbal Sánchez <cristobal.sanchez@globant.com>
2026-01-28 19:08:58 -0300

    Add model implementation. Add justification for implementation.

--- challenge/model.py
+++ challenge/model.py
@@ -1,13 +1,46 @@
-import pandas as pd
-
+from datetime import datetime
 from typing import Tuple, Union, List, Optional
 
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+
 class DelayModel:
+
+    TOP_10_FEATURES = [
+        "OPERA_Latin American Wings", 
+        "MES_7",
+        "MES_10",
+        "OPERA_Grupo LATAM",
+        "MES_12",
+        "TIPOVUELO_I",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air"
+    ]
+    THRESHOLD_IN_MINUTES = 15
+
 
     def __init__(
         self
     ):
-        self._model = None # Model should be saved in this attribute.
+        self._model = None
+
+    def _get_min_diff(self, row: pd.Series) -> float:
+        """
+        Gets the difference, in minutes, between the rows Fecha-O and Fecha-I
+        
+        Args:
+            row (pd.Series): raw row.
+        Returns:
+            float: minutes diference
+        """
+        fecha_o = datetime.strptime(row['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(row['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        return min_diff
 
     def preprocess(
         self,
@@ -26,7 +59,22 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        ...
+        # Generate features using one-hot encoding
+        features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix='OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'),
+            pd.get_dummies(data['MES'], prefix='MES')
+        ], axis=1)
+
+        features = features.reindex(columns=self.TOP_10_FEATURES, fill_value=0)
+
+        if target_column:
+            data['min_diff'] = data.apply(self._get_min_diff, axis = 1)
+            data['delay'] = np.where(data['min_diff'] > self.THRESHOLD_IN_MINUTES, 1, 0)
+            target = data[[target_column]]
+            return features, target
+        else:
+            return features
 
     def fit(
         self,
@@ -40,7 +88,16 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        # Class balance scale
+        target_values = target.values.ravel()
+        n_y0 = len(target_values[target_values == 0])
+        n_y1 = len(target_values[target_values == 1])
+        total = len(target_values)
+
+        self._model = LogisticRegression(
+            class_weight={0: n_y1 / total, 1: n_y0 / total}
+        )
+        self._model.fit(features, target_values)
 
     def predict(
         self,
@@ -55,4 +112,8 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        ...
+        if self._model is None:
+            return [0] * features.shape[0]
+        else: 
+            predictions = self._model.predict(features)
+            return [int(pred) for pred in predictions]
--- docs/challenge.md
+++ docs/challenge.md
@@ -1,5 +1,32 @@
 # Challenge
 
+## Part I (Model)
+
+According to the results:
+
+| Model | Accuracy | Recall (delay) | F1 (delay) |
+| -- | -- | -- | -- |
+| XGB without balance | 0.81 | 0.00 | 0.00 |
+| LogReg without balance | 0.81 | 0.03 | 0.06 |
+| XGB + top 10 features + balance | 0.55 | 0.69 | 0.37 |
+| XGB + top 10 features - balance | 0.81 |  0.01 | 0.01 |
+| LogReg + top 10 features + balance | 0.55 | 0.69 |   0.36 |
+|  LogReg + top 10 features - balance | 0.81 | 0.01 | 0.03 |
+
+The models with 81% of accuracy and zero recall are useless for detect delay. If there are 100 delayed flights and my recall is zero, my model detect none of them.
+Since I think delay detection is more important for LATAM's operations I will focus on higher recalls.
+
+| Model | Accuracy | Recall (delay) | F1 (delay) |
+| -- | -- | -- | -- |
+| XGB + top 10 features + balance | 0.55 | 0.69 | 0.37 |
+| LogReg + top 10 features + balance | 0.55 | 0.69 |   0.36 |
+
+Both models have similar performance (0.69), so with that in mind:
+
+* LogisticRegression is simpler and more interpretable.
+* There is no evidence that the additional complexity of XGBoost adds value.
+So using Occam's razor (aka principle of parsimony), I chose **LogisticRegression with top 10 features and class balancing**.
+
 ## Local execution
 
 ```bash
